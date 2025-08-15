fb46b4ddb04614f2256865f990bd78409684874f
akshaysyadav <165084134+akshaysyadav@users.noreply.github.com>
2025-08-16 01:03:32 +0530

    dashboard

--- Desktop/tradingplatform/dashboard/src/components/TopBar.js
+++ Desktop/tradingplatform/dashboard/src/components/TopBar.js
@@ -8,13 +8,13 @@ const TopBar = () => {
       <div className="indices-container">
         <div className="nifty">
           <p className="index">NIFTY 50</p>
-          <p className="index-points">{100.2} </p>
+          <p className="index-points">{24100.2} </p>
           <p className="percent"> </p>
         </div>
         <div className="sensex">
           <p className="index">SENSEX</p>
-          <p className="index-points">{100.2}</p>
-          <p className="percent"></p>
+          <p className="index-points">{81100.2}</p>
+          <p className="percent"> </p>
         </div>
       </div>
 
